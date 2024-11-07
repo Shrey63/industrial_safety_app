099aa5af2c684e0d90d33d8fc6994f94f9a4b815
Shreya Agrawal <shreyaparasagrawal@gmail.com>
2024-11-07 12:22:35 +0530

    Email setup in progress

--- industrial_safety_app/settings.py
+++ industrial_safety_app/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'industrial_safety_app'
+EMAIL_HOST_PASSWORD ='salanykxyvczkttl'
+EMAIL_USE_TLS=True
